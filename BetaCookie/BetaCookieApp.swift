ea99ac6d5fde7eac80c60696c6d5d278653581ce
Anjin Lee <anjin720jin@naver.com>
2024-10-20 14:38:37 +0900

    first commit

---
+++ BetaCookie/BetaCookieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaCookieApp.swift
+//  BetaCookie
+//
+//  Created by Anjin on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BetaCookieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
